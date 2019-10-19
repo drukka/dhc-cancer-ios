f675b4e0df48dce8a2544ead1a40ab602e997be5
Levente Dimény <>
2019-10-19 17:06:00 +0200

    Design xib file for SignUpViewController

---
+++ DHCCancer/Controllers/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  DHCCancer
+//
+//  Created by Levente Dimény on 2019. 10. 19..
+//  Copyright © 2019. Drukka Digitals. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
